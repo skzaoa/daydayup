bbbbcccb6c35c230f47d60fbf94630e2e6d6295a
skzaoa <2515259255@qq.com>
2019-12-01 12:31:55 +0800

    hello world

---
+++ study/src/com/sk/main/testClassMain.java
@@ -0,0 +1,7 @@
+package com.sk.main;
+
+public class testClassMain {
+    public static void main(String[] args) {
+        System.out.println("hello main");
+    }
+}
---
+++ study/src/com/sk/test/testClass.java
@@ -0,0 +1,8 @@
+package com.sk.test;
+
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
