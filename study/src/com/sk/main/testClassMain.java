e9f1c7093ef8c57712e2f169f6afe2494c86b178
skzaoa <2515259255@qq.com>
2019-12-01 13:52:18 +0800

    hello world

--- study/src/com/sk/main/testClassMain.java
+++ study/src/com/sk/main/testClassMain.java
@@ -1,7 +1,49 @@
+
 package com.sk.main;
+import com.sk.test.*;
+
+/**
+ * @author sk
+ */
 
 public class testClassMain {
+    private int id;
+    private String name;
+    private double height;
+
+    private int getId(){
+        return id;
+    }
+    private void setId(int id){
+        this.id = id;
+    }
+    private String getName(){
+        return name;
+    }
+    private void setName(String name){
+        this.name = name;
+    }
+    private double getHeight(){
+        return height;
+    }
+    private void setHeight(double height){
+        this.height = height;
+    }
+
+    private void testClassDisplay() {
+        System.out.println("id=" + getId());
+        System.out.println("height=" + getHeight());
+        System.out.println("name=" + getName());
+    }
     public static void main(String[] args) {
-        System.out.println("hello main");
+        testClassMain stClassMain = new testClassMain();
+        stClassMain.setId(1);
+        stClassMain.setHeight(177);
+        stClassMain.setName("sk");
+        stClassMain.testClassDisplay();
+
+        testClass stCLass = new testClass();
+        stCLass.setAge(18);
+        stCLass.testClassDisplay();
     }
 }
--- study/src/com/sk/test/testClass.java
+++ study/src/com/sk/test/testClass.java
@@ -1,8 +1,18 @@
 package com.sk.test;
 
-public class testClass {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
+/**
+ * @author sk
+ */
 
+public class testClass {
+    private int age;
+    private int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void testClassDisplay(){
+        System.out.println("age=" + getAge());
+    }
 }
