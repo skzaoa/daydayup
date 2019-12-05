3a22d024b5dd73d1e119953b3c8b1cec107f9147
skzaoa <2515259255@qq.com>
2019-12-05 23:09:18 +0800

    plane game

---
+++ study/src/com/sk/PlaneGame/Constant.java
@@ -0,0 +1,10 @@
+package com.sk.PlaneGame;
+
+/**
+ * @author sk
+ * create on  2019/12/5:22:08
+ */
+public class Constant {
+    public static final int GAME_WIDTH = 500;
+    public static final int GAME_HEIGHT = 500;
+}
--- study/src/com/sk/PlaneGame/Game.java
+++ study/src/com/sk/PlaneGame/Game.java
@@ -2,6 +2,8 @@ package com.sk.PlaneGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,13 +11,14 @@ import java.awt.event.WindowEvent;
  * @author sk
  * create on  2019/12/4:20:47
  */
-public class Game extends JFrame {
+public class Game extends Frame {
 
     Image fjImg = GameUtil.getImage("images/fj.png");
     Image bjImg = GameUtil.getImage("images/bj.jpg");
 
     //int fjX = 250,fjY = 250;
     Plane plane = new Plane(fjImg,250,250);
+    Shell[] shells = new Shell[50];
 
     public void paint(Graphics g){
         //自动被调用，g相当于一只画笔
@@ -30,6 +33,17 @@ public class Game extends JFrame {
         //fjX++;
         plane.drawSelf(g);
 
+        //shell.draw(g);
+        for(int i=0;i<shells.length;i++) {
+            shells[i].draw(g);
+
+            boolean peng = shells[i].getRect().intersects(plane.getRect());
+
+            if(peng) {
+                System.out.println("撞！！！");
+                plane.live = false;
+            }
+        }
         //g.setColor(c);
         //g.setFont(f);
     }
@@ -40,7 +54,7 @@ public class Game extends JFrame {
         public void run() {
             while(true){
                 repaint(); //重画
-                System.out.println("重画");
+                //System.out.println("重画");
                 try {
                     Thread.sleep(40);//1s25次
                 } catch (InterruptedException e) {
@@ -49,13 +63,28 @@ public class Game extends JFrame {
             }
         }
     }
+
+    //键盘监听内部类
+    class KeyMonitor extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            //System.out.println(e.getKeyCode());
+            plane.addDirection(e);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //System.out.println(e.getKeyCode());
+            plane.minusDirection(e);
+        }
+    }
     /**
      * 初始化窗口
      */
     public void launchFrame() {
         this.setTitle("游戏人生");
         this.setVisible(true);
-        this.setSize(500,500);
+        this.setSize(Constant.GAME_WIDTH,Constant.GAME_HEIGHT);
         this.setLocation(300,300);
 
         this.addWindowListener(new WindowAdapter() {
@@ -66,10 +95,26 @@ public class Game extends JFrame {
         });
 
         new PaintThread().start();//启动重画窗口的线程
+        addKeyListener(new KeyMonitor());//增加键盘监听
+
+        for(int i=0;i<shells.length;i++) {
+            shells[i] = new Shell();
+        }
     }
 
     public static void main(String[] agrs) {
         Game g = new Game();
         g.launchFrame();
+    }
+
+    private Image offScreenImage = null;
+
+    public void update(Graphics g) {
+        if(offScreenImage == null)
+            offScreenImage = this.createImage(Constant.GAME_WIDTH,Constant.GAME_HEIGHT);//这是游戏窗口的宽度和高度
+
+        Graphics gOff = offScreenImage.getGraphics();
+        paint(gOff);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 }
